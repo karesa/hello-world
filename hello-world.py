023162739215437baebe3c51f60923a893226330
karesa <38666928+karesa@users.noreply.github.com>
2020-06-04 20:20:08 -0700

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,7 @@
+def main ():
+
+    print("Hello World! My name is Karesa, I'm learning to code!")
+    print("Hola! Me llamo Karesa, Necesito practicar mi espanol tambien. ")
+
+    if __name__ == '__main__':
+        main()
